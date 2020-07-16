b7620da2a7525244ffa8fc6c0533f69dba61c2aa
Bohdan-Romanytsia <68371035+Bohdan-Romanytsia@users.noreply.github.com>
2020-07-16 11:24:29 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 echo 'Enter a filename, please'    
 read filename
-read -a strarr <<< '$filename'
+read -a strarr <<< "$filename"
 for (( i=0; i < ${#strarr[*]}; i++))
 do
-file='${strarr[i]}'
-    if [ -f '$file' ]
+file="${strarr[i]}"
+    if [ -f "$file" ]
         then
-            ls -l '$file'
-        else
-            echo '$file not regular file or does not exist.'
+            ls -l "$file"
     fi
 done
